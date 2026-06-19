ce274bdce9b291549b028fe92abbf38184cec0bc
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-19 16:32:33 +0300

    revert: take back the changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Navbar from "@/components/Navbar";
 import { useState } from "react";
 
 interface Verse {
@@ -120,8 +119,6 @@ export default function Page() {
   return (
     <div className="bg-sky-50 min-h-screen">
       <div className="max-w-3xl mx-auto flex items-center justify-center flex-col p-3 gap-3">
-        <Navbar />
-
         <section className="bg-white rounded-md p-3 flex flex-col sm:flex-row gap-3 w-full">
           {/* Book Select */}
           <select
