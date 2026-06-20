4c9e7227730f30e45205ac295b6dc2dcf19e6108
Myroslav Repin <myroslavrepin@gmail.com>
2026-06-20 11:52:55 -0600

    chore(cors): enable all origins CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.v1.verse import router as verse_router
 from backend.core.logger import logger
@@ -6,6 +7,15 @@ from backend.core.logger import logger
 app = FastAPI()
 
 app.include_router(verse_router, prefix="/api/v1", tags=["verse"])
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
